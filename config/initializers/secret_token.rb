9f65475b05ade86ae222379ae472037ddfa4e1f6
anthonybarone <barone42@gmail.com>
2013-10-10 22:56:39 -0400

    Ready to deploy VErsion 1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Healthether::Application.config.secret_key_base = '7ad1443198be73b4456659733be750212e07db59c70bd72267cf323648c9503b0376fc0a2e4f7dcc78f94b563be369d117d946304d5c377ae68e5ec973ed62d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Healthether::Application.config.session_store :cookie_store, key: '_healthether_session'

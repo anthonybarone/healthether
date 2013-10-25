0f253af307bfc587cffa6f2b903a608899bb8e56
anthonybarone <barone42@gmail.com>
2013-10-25 00:43:07 -0400

    formated content_area_1 and added footer content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,27 +5,35 @@ class PagesController < ApplicationController
   end
   
   def product_creator
+    @concepts = Concept.all
   end
   
   def product_types
+    @concepts = Concept.all
   end
   
   def product_specs
+    @concepts = Concept.all
   end
   
   def key_players
+    @concepts = Concept.all    
   end
   
   def contracts
+    @concepts = Concept.all    
   end
   
   def payments
+    @concepts = Concept.all    
   end
   
   def render_services
+    @concepts = Concept.all    
   end
   
   def maintenance
+    @concepts = Concept.all    
   end
   
 end

952bebb33b450b9b3291ba85c7797f341fd8a663
anthonybarone <barone42@gmail.com>
2013-11-02 02:20:50 -0400

    updated flashes css in comments.css.scss

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,19 @@ class CommentsController < ApplicationController
         @comment = Comment.new
   end
   
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to root_path, notice: "Thanks for the feedback!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:firstname, :lastname, :email, :note)
+  end
+  
 end
